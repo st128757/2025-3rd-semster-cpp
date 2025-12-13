b007d69947ccce3a8c729245550780a423336d1b
Vlad Tretyakov <st128757@student.spbu.ru>
2025-12-13 16:20:04 +0300

    Update Source.cpp

--- HW_4/Source.cpp
+++ HW_4/Source.cpp
@@ -6,5 +6,8 @@ int main() {
   String const hello("hello");
   String const ell = hello[1][4];
   ell.Print();
+  String result = ell;
+  String to_append = " there!";
+  result.append(to_append);
   return 0;
 }
